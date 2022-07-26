5e9c18aac8d6fee7b3639412cbb0f80c97a1a8c8
juenko <108847515+juen-k@users.noreply.github.com>
2022-07-25 21:37:04 -0500

    19.2 activities done

--- 19.2-week/01_Conditionals/unsolved/js/index.js
+++ 19.2-week/01_Conditionals/unsolved/js/index.js
@@ -1,3 +1,21 @@
 $('.tab').on("click", function(){
+    if ($(this).hasClass("tab1")) {
+        $(".tab1").addClass("active");
+        $(".tab2").removeClass("active");
+        $("#tabContent1").css("display", "block");
+        $("#tabContent2").css("display", "none");
+        
+        console.log("Tab 1 was selected")
+
+    }
+    else if ($(this).hasClass("tab2")) {
+        $(".tab2").addClass("active");
+        $(".tab1").removeClass("active");
+        $("#tabContent2").css("display", "block");
+        $("#tabContent1").css("display", "none");
+
+        console.log("Tab 2 was selected")
+
+    }
   
 });
--- 19.2-week/03_todo_app/unsolved/js/index.js
+++ 19.2-week/03_todo_app/unsolved/js/index.js
@@ -1,5 +1,33 @@
 // Create event listener to enter items below
+$("#enter").on("click", function() {
+    var task=$("#todoItem").val();
+    $("#todoList").append("<div class='task'>" + task + "<div class='x fas fa-times'></div></div>");
 
+    console.log("Someone added a task");
+});
 // Create event listener to delete items below
+$(document).on("click", ".x", function() {
+    $(this).parent().remove();
+
+    console.log("Someone removed a task")
+});
 
 // Create event listener to toggle 
+$(document).on("click", ".task", function() {
+    $(this).toggleClass("done");
+
+    if ($(this).hasClass("done")) {
+        $(this).find("div").removeClass("fa-times");
+        $(this).find("div").addClass("fa-check");
+
+        console.log("Someone marked a task completed")
+    }
+    
+    else {
+        $(this).find("div").addClass("fa-time")
+
+        console.log("Someone marked a task as incomplete");
+
+    };
+
+});
